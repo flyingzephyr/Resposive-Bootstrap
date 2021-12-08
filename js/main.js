014685706672363b042824cd113579079ad12110
Rezuanul Zannat <flyingzephyr@hotmail.com>
2021-12-09 02:38:13 +0600

    Gallery added

--- js/main.js
+++ js/main.js
@@ -3,4 +3,18 @@ $(document).ready(function() {
         arrows: false,
         dots: true,
     });
+
+    new VenoBox({
+        selector: '.video-link',
+        spinner: 'wave',
+        spinnerColor: '#e23e38',
+        overlayClose: false,
+    });
+
+    new VenoBox({
+        selector: '.light-box',
+        spinner: 'wave',
+        spinnerColor: '#e23e38',
+        overlayClose: true,
+    });
 });
