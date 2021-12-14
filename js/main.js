c54a39c122539c676ab17e5fdb668d23e9498b9b
Rezuanul Zannat <flyingzephyr@hotmail.com>
2021-12-15 01:30:20 +0600

    footer started

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
 $(document).ready(function() {
+
+    // slick-slider added
     $('.banner-slider').slick({
         arrows: false,
         dots: true,
     });
 
+    $('.test-slider').slick({
+        arrows: false,
+        dots: true,
+    });
+
+    $('.link-slider').slick({
+        arrows: true,
+        prevArrow: "<i class='fa fa-angle-left'></i>",
+        nextArrow: "<i class='fa fa-angle-right'></i>",
+        dots: false,
+        autoplay: true,
+    });
+
+    //  light-box added
     new VenoBox({
         selector: '.video-link',
         spinner: 'wave',
@@ -16,5 +32,22 @@ $(document).ready(function() {
         spinner: 'wave',
         spinnerColor: '#e23e38',
         overlayClose: true,
+    });
+
+    // counter added
+
+    $(".counter").counterUp({
+        delay: 20,
+        time: 1500,
+        offset: 100,
+    });
+
+    // mixitup
+    var mixer = mixitup('.grid');
+
+    //mixitup active btn with bootstrap
+    $('.active').addClass('mixitup-control-active');
+    jQuery(function() {
+        jQuery('#b-active').click();
     });
 });
